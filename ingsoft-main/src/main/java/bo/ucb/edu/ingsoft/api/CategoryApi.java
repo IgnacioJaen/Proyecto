82ab24ba553d58df692a92866d1d04c66ff750e3
XimeCruz <xmenacruzt@gmail.com>
2020-11-07 22:49:31 -0400

    get category probado y funcionando

--- ingsoft-main/src/main/java/bo/ucb/edu/ingsoft/api/CategoryApi.java
+++ ingsoft-main/src/main/java/bo/ucb/edu/ingsoft/api/CategoryApi.java
@@ -2,17 +2,22 @@ package bo.ucb.edu.ingsoft.api;
 
 import bo.ucb.edu.ingsoft.bl.CategoryBl;
 import bo.ucb.edu.ingsoft.model.Category;
+import org.mybatis.logging.Logger;
+import org.mybatis.logging.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping(value = "/v1/category")
 public class CategoryApi {
 
     private CategoryBl categoryBl;
+    private static final Logger LOGGER = LoggerFactory.getLogger(CategoryApi.class);
 
     @Autowired
     public CategoryApi(CategoryBl categoryBl) {
@@ -20,7 +25,7 @@ public class CategoryApi {
     }
 
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Category findById() {
+    public Category findById(HttpServletRequest request) {
         return categoryBl.findCategoryById(0);
     }
 
