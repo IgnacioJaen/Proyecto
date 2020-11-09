cab192a32a9aed1421f3d5fdc2b66288c0d946ee
Ro-Nel <69656274+Ro-Nel@users.noreply.github.com>
2020-11-09 15:25:37 -0400

    Clase Profile
    Corrección

--- ingsoft-main/src/main/java/bo/ucb/edu/ingsoft/model/Profile.java
+++ ingsoft-main/src/main/java/bo/ucb/edu/ingsoft/model/Profile.java
@@ -1,16 +1,10 @@
 package bo.ucb.edu.ingsoft.model;
 
-import java.util.Date;
-
 public class Profile {
     private Integer profileId;
     private Integer userId;
     private Integer status;
-    private Integer txId;
-    private String txHost;
-    private Integer txUserId;
-    private Date txDate;
-    private Date txUpdate;
+    private Transaction transaction;
 
     public Profile() {
     }
@@ -39,44 +33,12 @@ public class Profile {
         this.status = status;
     }
 
-    public Integer getTxId() {
-        return txId;
+    public Transaction getTransaction() {
+        return transaction;
     }
 
-    public void setTxId(Integer txId) {
-        this.txId = txId;
-    }
-
-    public String getTxHost() {
-        return txHost;
-    }
-
-    public void setTxHost(String txHost) {
-        this.txHost = txHost;
-    }
-
-    public Integer getTxUserId() {
-        return txUserId;
-    }
-
-    public void setTxUserId(Integer txUserId) {
-        this.txUserId = txUserId;
-    }
-
-    public Date getTxDate() {
-        return txDate;
-    }
-
-    public void setTxDate(Date txDate) {
-        this.txDate = txDate;
-    }
-
-    public Date getTxUpdate() {
-        return txUpdate;
-    }
-
-    public void setTxUpdate(Date txUpdate) {
-        this.txUpdate = txUpdate;
+    public void setTransaction(Transaction transaction) {
+        this.transaction = transaction;
     }
 
     @Override
@@ -85,11 +47,7 @@ public class Profile {
                 "profileId=" + profileId +
                 ", userId=" + userId +
                 ", status=" + status +
-                ", txId=" + txId +
-                ", txHost='" + txHost + '\'' +
-                ", txUserId=" + txUserId +
-                ", txDate=" + txDate +
-                ", txUpdate=" + txUpdate +
+                ", transaction=" + transaction +
                 '}';
     }
 }
