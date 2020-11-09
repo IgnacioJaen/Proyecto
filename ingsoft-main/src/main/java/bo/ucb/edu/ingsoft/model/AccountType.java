63e1846ba4b6704cc5801f83ef6dd91a5d0573f8
XimeCruz <xmenacruzt@gmail.com>
2020-11-09 19:30:56 -0400

    arreglando la doble transaccion categories y subcategories

--- ingsoft-main/src/main/java/bo/ucb/edu/ingsoft/model/AccountType.java
+++ ingsoft-main/src/main/java/bo/ucb/edu/ingsoft/model/AccountType.java
@@ -15,49 +15,33 @@ public class AccountType {
     }
 
 
-    public Integer getTxId() {
-        return transaction.getTxId();
-    }
+
 
     public void setTxId(Integer txId) {
         this.transaction.setTxId(txId);
     }
 
-    public String getTxHost() {
-        return transaction.getTxHost();
-    }
 
     public void setTxHost(String txHost) {
         this.transaction.setTxHost(txHost);
     }
 
-    public Integer getTxUserId() {
-        return transaction.getTxUserId();
-    }
 
     public void setTxUserId(Integer txUserId) {
         this.transaction.setTxUserId(txUserId);
     }
 
-    public Date getTxDate() {
-        return transaction.getTxDate();
-    }
+
 
     public void setTxDate(Date txDate) {
         this.transaction.setTxDate(txDate);
     }
 
-    public Date getTxUpdate() {
-        return transaction.getTxUpdate();
-    }
 
     public void setTxUpdate(Date txUpdate) {
         this.transaction.setTxUpdate(txUpdate);
     }
 
-    public Integer getAccountTypeId() {
-        return accountTypeId;
-    }
 
     public void setAccountTypeId(Integer accountTypeId) {
         this.accountTypeId = accountTypeId;
--- ingsoft-main/src/main/java/bo/ucb/edu/ingsoft/model/Category.java
+++ ingsoft-main/src/main/java/bo/ucb/edu/ingsoft/model/Category.java
@@ -45,41 +45,30 @@ public class Category {
         this.transaction = transaction;
     }
 
-    public Integer getTxId() {
-        return transaction.getTxId();
-    }
 
     public void setTxId(Integer txId) {
         this.transaction.setTxId(txId);
     }
 
-    public String getTxHost() {
-        return transaction.getTxHost();
-    }
+
 
     public void setTxHost(String txHost) {
         this.transaction.setTxHost(txHost);
     }
 
-    public Integer getTxUserId() {
-        return transaction.getTxUserId();
-    }
+
 
     public void setTxUserId(Integer txUserId) {
         this.transaction.setTxUserId(txUserId);
     }
 
-    public Date getTxDate() {
-        return transaction.getTxDate();
-    }
+
 
     public void setTxDate(Date txDate) {
         this.transaction.setTxDate(txDate);
     }
 
-    public Date getTxUpdate() {
-        return transaction.getTxUpdate();
-    }
+
 
     public void setTxUpdate(Date txUpdate) {
         this.transaction.setTxUpdate(txUpdate);
