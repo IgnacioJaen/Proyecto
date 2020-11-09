dd601db2fb88dee26d8b932a75506168ffb52ec7
Ro-Nel <69656274+Ro-Nel@users.noreply.github.com>
2020-11-09 16:35:52 -0400

    Clase ProfileDao
    añadiendo a ProfileDao

--- ingsoft-main/src/main/java/bo/ucb/edu/ingsoft/bl/ProfileBl.java
+++ ingsoft-main/src/main/java/bo/ucb/edu/ingsoft/bl/ProfileBl.java
@@ -8,5 +8,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProfileBl {
 
+    private ProfileDao profileDao;
 
+    @Autowired
+    public ProfileBl(ProfileDao profileDao) {
+        this.profileDao = profileDao;
+    }
+
+    public ProfileRequest profileget(String nameUser, String surnameUser) {
+        return profileDao.profilegetp(nameUser,surnameUser);
+    }
 }
