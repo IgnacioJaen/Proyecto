056d4df2a6df2c366bfa20e10f221c2411b0e6bb
Ro-Nel <69656274+Ro-Nel@users.noreply.github.com>
2020-11-09 16:38:42 -0400

    Clase ProfileApi
    Completando ProfileApi

--- ingsoft-main/src/main/java/bo/ucb/edu/ingsoft/api/ProfileApi.java
+++ ingsoft-main/src/main/java/bo/ucb/edu/ingsoft/api/ProfileApi.java
@@ -1,4 +1,5 @@
 package bo.ucb.edu.ingsoft.api;
+
 import bo.ucb.edu.ingsoft.bl.ProfileBl;
 import bo.ucb.edu.ingsoft.dto.ProfileRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,16 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping(value = "/v1/profile")
 public class ProfileApi {
 
+    private ProfileBl profileBl;
+
+    @Autowired
+    public ProfileApi(ProfileBl profileBl) {
+        this.profileBl = profileBl;
+    }
 
 
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ProfileRequest findById(@RequestBody String nameUser, String surnameUser,HttpServletRequest request) {
+        return profileBl.profileget(nameUser,surnameUser);
+    }
 }
