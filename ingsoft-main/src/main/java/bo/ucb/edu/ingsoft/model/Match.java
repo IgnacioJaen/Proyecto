efea48064a844144461563bc567d4caa0e70abfd
Ro-Nel <69656274+Ro-Nel@users.noreply.github.com>
2020-11-09 15:16:33 -0400

    Clase Photos
    Mejorado los tostring

--- ingsoft-main/src/main/java/bo/ucb/edu/ingsoft/model/Match.java
+++ ingsoft-main/src/main/java/bo/ucb/edu/ingsoft/model/Match.java
@@ -1,56 +1,39 @@
 package bo.ucb.edu.ingsoft.model;
 
+import java.util.Date;
 
-public class Match {
-    private Integer matchId;
-    private Integer profile1Id;
-    private Integer profile2Id;
-    private Integer dateMatches;
-    private Integer accepted;
+public class Photos {
+    private Integer photosId;
+    private Integer categoryId;
+    private String photoPath;
     private Integer status;
     private Transaction transaction;
 
-    public Match() {
+    public Photos() {
     }
 
-    public Integer getMatchId() {
-        return matchId;
+    public Integer getPhotosId() {
+        return photosId;
     }
 
-    public void setMatchId(Integer matchId) {
-        this.matchId = matchId;
+    public void setPhotosId(Integer photosId) {
+        this.photosId = photosId;
     }
 
-    public Integer getProfile1Id() {
-        return profile1Id;
+    public Integer getCategoryId() {
+        return categoryId;
     }
 
-    public void setProfile1Id(Integer profile1Id) {
-        this.profile1Id = profile1Id;
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
     }
 
-    public Integer getProfile2Id() {
-        return profile2Id;
+    public String getPhotoPath() {
+        return photoPath;
     }
 
-    public void setProfile2Id(Integer profile2Id) {
-        this.profile2Id = profile2Id;
-    }
-
-    public Integer getDateMatches() {
-        return dateMatches;
-    }
-
-    public void setDateMatches(Integer dateMatches) {
-        this.dateMatches = dateMatches;
-    }
-
-    public Integer getAccepted() {
-        return accepted;
-    }
-
-    public void setAccepted(Integer accepted) {
-        this.accepted = accepted;
+    public void setPhotoPath(String photoPath) {
+        this.photoPath = photoPath;
     }
 
     public Integer getStatus() {
@@ -71,12 +54,10 @@ public class Match {
 
     @Override
     public String toString() {
-        return "Match{" +
-                "matchId=" + matchId +
-                ", profile1Id=" + profile1Id +
-                ", profile2Id=" + profile2Id +
-                ", dateMatches=" + dateMatches +
-                ", accepted=" + accepted +
+        return "Photos{" +
+                "photosId=" + photosId +
+                ", categoryId=" + categoryId +
+                ", photoPath='" + photoPath + '\'' +
                 ", status=" + status +
                 ", transaction=" + transaction +
                 '}';
