5f4c4af78cd29862d805d99b4334ec4676c85f7e
XimeCruz <xmenacruzt@gmail.com>
2020-11-10 01:26:06 -0400

    base de datos en orden

--- ingsoft-main/no_appearance.sql
+++ ingsoft-main/no_appearance.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-11-2020 a las 12:39:47
+-- Tiempo de generación: 10-11-2020 a las 01:05:09
 -- Versión del servidor: 10.1.37-MariaDB
 -- Versión de PHP: 5.6.40
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `no_appearance`
+-- Base de datos: `no_app`
 --
 
 -- --------------------------------------------------------
@@ -123,7 +123,7 @@ CREATE TABLE `h_user` (
   `name` varchar(100) NOT NULL,
   `surname` varchar(100) NOT NULL,
   `birthdate` date NOT NULL,
-  `gender` tinyint(1) NOT NULL,
+  `gender` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `user_photo` varchar(255) NOT NULL,
@@ -148,7 +148,6 @@ CREATE TABLE `match` (
   `profile1_id` int(11) NOT NULL,
   `profile2_id` int(11) NOT NULL,
   `date_matches` date NOT NULL,
-  `accepted` tinyint(1) NOT NULL,
   `status` int(11) NOT NULL,
   `tx_id` int(11) NOT NULL,
   `tx_host` varchar(100) NOT NULL,
@@ -161,17 +160,17 @@ CREATE TABLE `match` (
 -- Volcado de datos para la tabla `match`
 --
 
-INSERT INTO `match` (`match_id`, `profile1_id`, `profile2_id`, `date_matches`, `accepted`, `status`, `tx_id`, `tx_host`, `tx_user`, `tx_date`, `tx_update`) VALUES
-(11, 1, 10, '2020-10-29', 1, 1, 2, '192.168.31.148', 12, '2020-10-30 02:10:19', '2020-10-29'),
-(12, 2, 9, '2020-10-29', 1, 1, 2, '192.168.31.148', 12, '2020-10-30 02:10:19', '2020-10-29'),
-(13, 3, 8, '2020-10-29', 0, 1, 2, '192.168.31.148', 12, '2020-10-30 02:10:19', '2020-10-29'),
-(14, 4, 7, '2020-10-29', 1, 1, 2, '192.168.31.148', 12, '2020-10-30 02:10:19', '2020-10-29'),
-(15, 5, 6, '2020-10-29', 1, 1, 2, '192.168.31.148', 12, '2020-10-30 02:10:19', '2020-10-29'),
-(16, 6, 5, '2020-10-29', 0, 1, 2, '192.168.31.148', 12, '2020-10-30 02:10:19', '2020-10-29'),
-(17, 7, 4, '2020-10-29', 1, 1, 2, '192.168.31.148', 12, '2020-10-30 02:10:19', '2020-10-29'),
-(18, 8, 3, '2020-10-29', 0, 1, 2, '192.168.31.148', 12, '2020-10-30 02:10:19', '2020-10-29'),
-(19, 9, 2, '2020-10-29', 0, 1, 2, '192.168.31.148', 12, '2020-10-30 02:10:19', '2020-10-29'),
-(20, 10, 1, '2020-10-29', 1, 1, 2, '192.168.31.148', 12, '2020-10-30 02:10:19', '2020-10-29');
+INSERT INTO `match` (`match_id`, `profile1_id`, `profile2_id`, `date_matches`, `status`, `tx_id`, `tx_host`, `tx_user`, `tx_date`, `tx_update`) VALUES
+(11, 1, 10, '2020-10-29', 1, 2, '192.168.31.148', 12, '2020-10-30 02:10:19', '2020-10-29'),
+(12, 2, 9, '2020-10-29', 1, 2, '192.168.31.148', 12, '2020-10-30 02:10:19', '2020-10-29'),
+(13, 3, 8, '2020-10-29', 0, 2, '192.168.31.148', 12, '2020-10-30 02:10:19', '2020-10-29'),
+(14, 4, 7, '2020-10-29', 1, 2, '192.168.31.148', 12, '2020-10-30 02:10:19', '2020-10-29'),
+(15, 5, 6, '2020-10-29', 1, 2, '192.168.31.148', 12, '2020-10-30 02:10:19', '2020-10-29'),
+(16, 6, 5, '2020-10-29', 0, 2, '192.168.31.148', 12, '2020-10-30 02:10:19', '2020-10-29'),
+(17, 7, 4, '2020-10-29', 1, 2, '192.168.31.148', 12, '2020-10-30 02:10:19', '2020-10-29'),
+(18, 8, 3, '2020-10-29', 0, 2, '192.168.31.148', 12, '2020-10-30 02:10:19', '2020-10-29'),
+(19, 9, 2, '2020-10-29', 0, 2, '192.168.31.148', 12, '2020-10-30 02:10:19', '2020-10-29'),
+(20, 10, 1, '2020-10-29', 1, 2, '192.168.31.148', 12, '2020-10-30 02:10:19', '2020-10-29');
 
 -- --------------------------------------------------------
 
@@ -312,74 +311,6 @@ INSERT INTO `profile_subcategory` (`profile_sub_id`, `profile_id`, `subcategory_
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `report`
---
-
-CREATE TABLE `report` (
-  `report_id` int(11) NOT NULL,
-  `chat_id` int(11) NOT NULL,
-  `status` int(11) NOT NULL,
-  `tx_id` int(11) NOT NULL,
-  `tx_host` varchar(100) NOT NULL,
-  `tx_user` int(11) NOT NULL,
-  `tx_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `tx_update` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Registered users in the app';
-
---
--- Volcado de datos para la tabla `report`
---
-
-INSERT INTO `report` (`report_id`, `chat_id`, `status`, `tx_id`, `tx_host`, `tx_user`, `tx_date`, `tx_update`) VALUES
-(1, 1, 1, 13, '192.168.1.1', 1, '2020-11-07 16:36:27', '2020-10-29'),
-(2, 5, 0, 11, '192.168.1.1', 1, '2020-11-07 16:36:43', '2020-10-29'),
-(3, 6, 1, 14, '192.168.1.1', 1, '2020-11-07 16:36:43', '2020-10-29'),
-(4, 8, 0, 14, '192.168.1.1', 1, '2020-11-07 16:36:43', '2020-10-29'),
-(5, 9, 1, 11, '192.168.1.1', 1, '2020-11-07 16:36:43', '2020-10-29'),
-(6, 10, 0, 14, '192.168.1.1', 1, '2020-11-07 16:36:43', '2020-10-29'),
-(7, 1, 1, 12, '192.168.1.1', 1, '2020-11-07 16:36:43', '2020-10-29'),
-(8, 3, 0, 11, '192.168.1.1', 1, '2020-11-07 16:36:43', '2020-10-29'),
-(9, 2, 1, 12, '192.168.1.1', 1, '2020-11-07 16:36:43', '2020-10-29'),
-(10, 4, 0, 14, '192.168.1.1', 1, '2020-11-07 16:36:43', '2020-10-29');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `report_options`
---
-
-CREATE TABLE `report_options` (
-  `report_op_id` int(11) NOT NULL,
-  `report_id` int(11) NOT NULL,
-  `message_id` int(11) NOT NULL,
-  `description` varchar(150) NOT NULL,
-  `status` int(11) NOT NULL,
-  `tx_id` int(11) NOT NULL,
-  `tx_host` varchar(100) NOT NULL,
-  `tx_user` int(11) NOT NULL,
-  `tx_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `tx_update` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `report_options`
---
-
-INSERT INTO `report_options` (`report_op_id`, `report_id`, `message_id`, `description`, `status`, `tx_id`, `tx_host`, `tx_user`, `tx_date`, `tx_update`) VALUES
-(1, 1, 5, '5', 1, 13, '192.168.1.1', 1, '2020-11-07 16:37:37', '2020-10-29'),
-(2, 5, 6, '4', 0, 11, '192.168.1.1', 1, '2020-11-07 16:38:10', '2020-10-29'),
-(3, 6, 4, '7', 1, 14, '192.168.1.1', 1, '2020-11-07 16:38:26', '2020-10-29'),
-(4, 8, 2, '6', 0, 14, '192.168.1.1', 1, '2020-11-07 16:38:26', '2020-10-29'),
-(5, 9, 3, '3', 1, 11, '192.168.1.1', 1, '2020-11-07 16:38:26', '2020-10-29'),
-(6, 10, 8, '1', 0, 14, '192.168.1.1', 1, '2020-11-07 16:38:26', '2020-10-29'),
-(7, 1, 6, '2', 1, 12, '192.168.1.1', 1, '2020-11-07 16:38:26', '2020-10-29'),
-(8, 3, 7, '6', 0, 11, '192.168.1.1', 1, '2020-11-07 16:38:26', '2020-10-29'),
-(9, 2, 4, '7', 1, 12, '192.168.1.1', 1, '2020-11-07 16:38:26', '2020-10-29'),
-(10, 4, 2, '5', 0, 14, '192.168.1.1', 1, '2020-11-07 16:38:26', '2020-10-29');
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `subcategory`
 --
 
@@ -424,7 +355,7 @@ CREATE TABLE `user` (
   `name` varchar(100) NOT NULL,
   `surname` varchar(100) NOT NULL,
   `birthdate` date NOT NULL,
-  `gender` tinyint(1) NOT NULL,
+  `gender` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `user_photo` varchar(255) NOT NULL,
@@ -441,20 +372,20 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `account_type_id`, `user_type_id`, `name`, `surname`, `birthdate`, `gender`, `email`, `password`, `user_photo`, `status`, `tx_id`, `tx_host`, `tx_user`, `tx_date`, `tx_update`) VALUES
-(1, 1, 1, 'Jose', 'Chura', '1998-10-10', 0, 'jc@gmail.com', '12345', 'URL', 1, 1, '192.168.15.25', 13, '2020-10-30 02:09:03', '2020-10-29'),
-(2, 2, 1, 'Andres', 'Roque', '1997-09-03', 0, 'ar@gmail.com', '48944', 'URL', 1, 1, '192.168.15.25', 13, '2020-10-30 02:09:03', '2020-10-29'),
-(3, 1, 1, 'Manuel', 'Melez', '1996-08-04', 0, 'mmg@gmail.com', 'ewa456', 'URL', 1, 1, '192.168.15.25', 13, '2020-10-30 02:09:03', '2020-10-29'),
-(4, 2, 1, 'Sophia', 'Rich', '1995-07-05', 0, 'sr@gmail.com', 'fas489', 'URL', 1, 1, '192.168.15.25', 13, '2020-10-30 02:09:03', '2020-10-29'),
-(5, 1, 1, 'Rene', 'Menji', '1994-06-11', 0, 'rm@gmail.com', 'ee1264', 'URL', 1, 1, '192.168.15.25', 13, '2020-10-30 02:09:03', '2020-10-29'),
-(6, 2, 1, 'Alejandra', 'Savedra', '2000-05-09', 0, 'as@gmail.com', 'das451', 'URL', 1, 1, '192.168.15.25', 13, '2020-10-30 02:09:03', '2020-10-29'),
-(7, 1, 1, 'Maria', 'Avaroa', '2001-10-05', 0, 'ma@gmail.com', 'vcx56', 'URL', 1, 1, '192.168.15.25', 13, '2020-10-30 02:09:03', '2020-10-29'),
-(8, 2, 1, 'Jhon', 'Perez', '1998-09-04', 0, 'jps@gmail.com', 'vdsvds44', 'URL', 1, 1, '192.168.15.25', 13, '2020-10-30 02:09:03', '2020-10-29'),
-(9, 1, 1, 'Ibonne', 'Chon', '1998-11-03', 0, 'df@gmail.com', 'bea56', 'URL', 1, 1, '192.168.15.25', 13, '2020-10-30 02:09:03', '2020-10-29'),
-(10, 2, 1, 'Tania', 'Dumphy', '1997-10-02', 0, 'jtdg@gmail.com', 'feacs55', 'URL', 1, 1, '192.168.15.25', 13, '2020-10-30 02:09:03', '2020-10-29'),
-(11, 1, 2, 'Ignacio', 'Jaen', '1996-01-01', 0, 'ignajaen@gmail.com', 'sacvasv99', 'URL', 1, 1, '192.168.15.25', 13, '2020-10-30 02:09:03', '2020-10-29'),
-(12, 1, 2, 'Marioly', 'Vargas', '1990-02-12', 0, 'mollyv@gmail.com', 'vasvas54', 'URL', 1, 1, '192.168.15.25', 13, '2020-10-30 02:09:03', '2020-10-29'),
-(13, 1, 2, 'Rodrigo', 'Saravia', '1980-03-11', 0, 'rodriSa@gmail.com', '12sfs345', 'URL', 1, 1, '192.168.15.25', 13, '2020-10-30 02:09:03', '2020-10-29'),
-(14, 1, 2, 'Ximena', 'Cruz', '1988-04-05', 0, 'ximenaC@gmail.com', 'mbrewq48', 'URL', 1, 1, '192.168.15.25', 13, '2020-10-30 02:09:03', '2020-10-29');
+(1, 1, 1, 'Jose', 'Chura', '1998-10-10', 'Masculino', 'jc@gmail.com', '12345', 'URL', 1, 1, '192.168.15.25', 13, '2020-10-30 02:09:03', '2020-10-29'),
+(2, 2, 1, 'Andres', 'Roque', '1997-09-03', 'Masculino', 'ar@gmail.com', '48944', 'URL', 1, 1, '192.168.15.25', 13, '2020-10-30 02:09:03', '2020-10-29'),
+(3, 1, 1, 'Manuel', 'Melez', '1996-08-04', 'Masculino', 'mmg@gmail.com', 'ewa456', 'URL', 1, 1, '192.168.15.25', 13, '2020-10-30 02:09:03', '2020-10-29'),
+(4, 2, 1, 'Sophia', 'Rich', '1995-07-05', 'Femenino', 'sr@gmail.com', 'fas489', 'URL', 1, 1, '192.168.15.25', 13, '2020-10-30 02:09:03', '2020-10-29'),
+(5, 1, 1, 'Rene', 'Menji', '1994-06-11', 'Masculino', 'rm@gmail.com', 'ee1264', 'URL', 1, 1, '192.168.15.25', 13, '2020-10-30 02:09:03', '2020-10-29'),
+(6, 2, 1, 'Alejandra', 'Savedra', '2000-05-09', 'Femenino', 'as@gmail.com', 'das451', 'URL', 1, 1, '192.168.15.25', 13, '2020-10-30 02:09:03', '2020-10-29'),
+(7, 1, 1, 'Maria', 'Avaroa', '2001-10-05', 'Femenino', 'ma@gmail.com', 'vcx56', 'URL', 1, 1, '192.168.15.25', 13, '2020-10-30 02:09:03', '2020-10-29'),
+(8, 2, 1, 'Jhon', 'Perez', '1998-09-04', 'Masculino', 'jps@gmail.com', 'vdsvds44', 'URL', 1, 1, '192.168.15.25', 13, '2020-10-30 02:09:03', '2020-10-29'),
+(9, 1, 1, 'Ibonne', 'Chon', '1998-11-03', 'Femenino', 'df@gmail.com', 'bea56', 'URL', 1, 1, '192.168.15.25', 13, '2020-10-30 02:09:03', '2020-10-29'),
+(10, 2, 1, 'Tania', 'Dumphy', '1997-10-02', 'Femenino', 'jtdg@gmail.com', 'feacs55', 'URL', 1, 1, '192.168.15.25', 13, '2020-10-30 02:09:03', '2020-10-29'),
+(11, 1, 2, 'Ignacio', 'Jaen', '1996-01-01', 'Masculino', 'ignajaen@gmail.com', 'sacvasv99', 'URL', 1, 1, '192.168.15.25', 13, '2020-10-30 02:09:03', '2020-10-29'),
+(12, 1, 2, 'Marioly', 'Vargas', '1990-02-12', 'Femenino', 'mollyv@gmail.com', 'vasvas54', 'URL', 1, 1, '192.168.15.25', 13, '2020-10-30 02:09:03', '2020-10-29'),
+(13, 1, 2, 'Rodrigo', 'Saravia', '1980-03-11', 'Masculino', 'rodriSa@gmail.com', '12sfs345', 'URL', 1, 1, '192.168.15.25', 13, '2020-10-30 02:09:03', '2020-10-29'),
+(14, 1, 2, 'Ximena', 'Cruz', '1988-04-05', 'Femenino', 'ximenaC@gmail.com', 'mbrewq48', 'URL', 1, 1, '192.168.15.25', 13, '2020-10-30 02:09:03', '2020-10-29');
 
 -- --------------------------------------------------------
 
@@ -480,6 +411,69 @@ CREATE TABLE `user_type` (
 INSERT INTO `user_type` (`user_type_id`, `type`, `status`, `tx_id`, `tx_host`, `tx_user_id`, `tx_date`, `tx_update`) VALUES
 (1, '1', 1, 1, '192.168.1.65', 11, '2020-10-30 02:09:02', '2020-10-29'),
 (2, '2', 1, 1, '192.168.1.65', 11, '2020-10-30 02:09:02', '2020-10-29');
+
+--
+-- Estructura de tabla para la tabla `report_options`
+--
+
+CREATE TABLE `report_options` (
+  `report_op_id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `description` varchar(150) NOT NULL,
+  `status` int(11) NOT NULL,
+  `tx_id` int(11) NOT NULL,
+  `tx_host` varchar(100) NOT NULL,
+  `tx_user` int(11) NOT NULL,
+  `tx_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `tx_update` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `report_options`
+--
+
+INSERT INTO `report_options` (`report_op_id`, `description`, `status`, `tx_id`, `tx_host`, `tx_user`, `tx_date`, `tx_update`) VALUES
+(1, "Mal vocabualario", 1, 13, '192.168.1.1', 1, '2020-11-07 16:37:37', '2020-10-29'),
+(2, "No busca una amistad", 0, 11, '192.168.1.1', 1, '2020-11-07 16:38:10', '2020-10-29'),
+(3, "Hostigador", 1, 14, '192.168.1.1', 1, '2020-11-07 16:38:26', '2020-10-29');
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `report`
+--
+
+CREATE TABLE `report` (
+  `report_id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+   `report_op_id` int(11) NOT NULL,
+  `chat_id` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  `tx_id` int(11) NOT NULL,
+  `tx_host` varchar(100) NOT NULL,
+  `tx_user` int(11) NOT NULL,
+  `tx_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `tx_update` date NOT NULL,
+   FOREIGN KEY (report_op_id) REFERENCES report_options(report_op_id),
+    FOREIGN KEY (chat_id) REFERENCES chat(chat_id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Registered users in the app';
+
+--
+-- Volcado de datos para la tabla `report`
+--
+
+INSERT INTO `report` (`report_id`, `report_op_id`, `chat_id`, `status`, `tx_id`, `tx_host`, `tx_user`, `tx_date`, `tx_update`) VALUES
+(1, 1, 1, 1, 13, '192.168.1.1', 1, '2020-11-07 16:36:27', '2020-10-29'),
+(2, 2, 5, 0, 11, '192.168.1.1', 1, '2020-11-07 16:36:43', '2020-10-29'),
+(3, 1, 4, 1, 14, '192.168.1.1', 1, '2020-11-07 16:36:43', '2020-10-29'),
+(4, 3, 8, 0, 14, '192.168.1.1', 1, '2020-11-07 16:36:43', '2020-10-29'),
+(5, 1, 9, 1, 11, '192.168.1.1', 1, '2020-11-07 16:36:43', '2020-10-29'),
+(6, 1, 10, 0, 14, '192.168.1.1', 1, '2020-11-07 16:36:43', '2020-10-29'),
+(7, 2, 1, 1, 12, '192.168.1.1', 1, '2020-11-07 16:36:43', '2020-10-29'),
+(8, 2, 3, 0, 11, '192.168.1.1', 1, '2020-11-07 16:36:43', '2020-10-29'),
+(9, 3, 2, 1, 12, '192.168.1.1', 1, '2020-11-07 16:36:43', '2020-10-29'),
+(10, 1, 4, 0, 14, '192.168.1.1', 1, '2020-11-07 16:36:43', '2020-10-29');
+
+-- --------------------------------------------------------
 
 --
 -- Índices para tablas volcadas
