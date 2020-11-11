4be3803e3f76de6a8a483e123c61f43492b01219
Marioly V <mariolyv7@gmail.com>
2020-11-11 14:07:28 -0400

    Agregando comentarios a los metodos de las clases AccountTypeDao, CategoryDao, ChatDao, MessagesDao, ProfileDao en la carpeta Dao

--- ingsoft-main/src/main/java/bo/ucb/edu/ingsoft/dao/MessagesDao.java
+++ ingsoft-main/src/main/java/bo/ucb/edu/ingsoft/dao/MessagesDao.java
@@ -6,16 +6,26 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface MessagesDao {
+    //Metodos de la clase MessagesDao que seran utilizadas por los .xml de la
+    //carpeta resources/dao que utilicen como mapper esta clase
 
+    //Metodo para encontrar un mensaje y tiene un parametro de tipo messages
     public Messages findMessagesById(Messages messages);
 
+    //Metodo para encontrar un mensaje y tiene un parametro de tipo messagesRequest
+    //ya que es el request para la vista del usuario de tipo cliente
     public MessagesRequest findMessagesReqById(MessagesRequest messagesRequest);
+
     /*
     public MessagesRequest findMessagesReqById(String date, String content);
      */
+
+    //Metodo para agregar un mensaje y tiene un parametro de tipo messages
     public void messagesInsert(Messages messages);
 
+    //Metodo para actualizar un mensaje y tiene un parametro de tipo messages
     public void messagesUpdate(Messages messages);
 
+    //Metodo para eliminar un mensaje y tiene un parametro de tipo messages
     public void messagesDelete(Messages messages);
 }
--- ingsoft-main/src/main/java/bo/ucb/edu/ingsoft/dao/ProfileDao.java
+++ ingsoft-main/src/main/java/bo/ucb/edu/ingsoft/dao/ProfileDao.java
@@ -5,6 +5,9 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface ProfileDao {
+    //Metodo de la clase ProfileDao que sera utilizada por los .xml de la
+    //carpeta resources/dao que utilicen como mapper esta clase
 
+    //Metodo para encontrar un perfil y tiene un parametro de tipo Integer
     ProfileRequest profilegetp(Integer userId);
 }
